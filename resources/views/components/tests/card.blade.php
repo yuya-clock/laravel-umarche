948004674443616bf0171e1b1b56a29a4262dc11
yuya-clock <yuya89t@gmail.com>
2022-02-24 07:37:00 +0900

    attributes merge

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,7 @@
     'message' => 'え？',
 ])
 
-<div class="border-2 shadow-md w-1/2 p-2">
+<div {{ $attributes->merge(['class' => 'border-2 shadow-md w-1/2 p-2']) }}>
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
