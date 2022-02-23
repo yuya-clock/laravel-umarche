c4dd8c141cb4912b887b788d228d937e421bc974
yuya-clock <yuya89t@gmail.com>
2022-02-24 07:20:13 +0900

    add props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,10 @@
-<div class="border-2 shadow-md w-1/4 p-2">
+@props([
+    'title',
+    'content' => 'キャプテンファルコンの得意技にはセリフがある',
+    'message' => 'え？',
+])
+
+<div class="border-2 shadow-md w-1/2 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,5 +4,6 @@
     </x-slot>
     component-test1
 
-    <x-tests.card title="タイトル" content="本文" :message="$goku_dialogue"/>
+    <x-tests.card title="タイトル1" content="本文" :message="$goku_dialogue"/>
+    <x-tests.card title="タイトル2"/>
 </x-tests.app>
