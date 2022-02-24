49a6bdd35267c31b5d88fbbc2defbcdac7a3afbf
yuya-clock <yuya89t@gmail.com>
2022-02-25 08:51:46 +0900

    class component

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです。
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
         header2
     </x-slot>
     component-test2
+    <x-test-class-base classBaseMessage="クラスベースメッセージだよ？"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="クラスベースメッセージだよ？" defaultMessage="初期値から変更したメッセージだぞ？"/>
 </x-tests.app>
